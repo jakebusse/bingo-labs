d635d9a90ff9254e0a3aea107a2401a5a6c6c939
Jake Busse <78458447+jakebusse@users.noreply.github.com>
2025-02-08 20:40:12 -0600

    fix eslint again

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 // File: app/page.tsx
-import { neon } from "@neondatabase/serverless";
+// import { neon } from "@neondatabase/serverless";
 
 export default function Page() {
   return <div></div>;
